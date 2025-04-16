4265e9a30f893a2d6d41f118fa15e60c88e09747
Lim-taeyoung <ggty89@gmail.com>
2025-04-16 23:01:19 +0900

    소통방 오류 수정

--- database.py
+++ database.py
@@ -4,14 +4,24 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./mydb.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+DATABASE_URL = "sqlite:///./excel_data.db"
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 
 # 👇 여기 선언
 Base = declarative_base()
 
 # 👇 이거 추가 (명시적 export)
 __all__ = ["Base", "engine", "SessionLocal"]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# 테이블 생성
+Base.metadata.create_all(bind=engine)
